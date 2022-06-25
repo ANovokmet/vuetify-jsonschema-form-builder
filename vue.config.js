ed0b314418e4be25e925208db497b1be8b82a56a
ANovokmet <ante.je.zakon@gmail.com>
2022-06-25 21:04:22 +0200

    gh pages fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+const production = process.env.NODE_ENV === 'production';
+
 module.exports = {
+  publicPath: production
+      ? '/vuetify-jsonschema-form-builder/'
+      : '/',
+  outputDir: 'dist',
   transpileDependencies: [
     'vuetify', '@koumoul/vjsf'
   ]
